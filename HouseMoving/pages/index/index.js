388cf18b57f0ff2bfe5496e2d21b8213800fcf09
meHoLi <346857553@qq.com>
2018-06-20 11:14:19 +0800

    radio更改事件

--- HouseMoving/pages/index/index.js
+++ HouseMoving/pages/index/index.js
@@ -4,18 +4,38 @@ const app = getApp()
 
 Page({
   data: {
-    carExplainTop:'计价：起步1288元(10公里)，超出10元/公里',
-    carExplainBottom: '容量：可容纳30个包裹，单个包裹限重100kg',
+    carExplainTop:'计价：起步208元(10公里)，超出5元/公里',
+    carExplainBottom: '容量：可容纳10个包裹，单个包裹限重15公斤',
     items: [
-      { name: 'car1', value: '小面' },
-      { name: 'car2', value: '金杯', checked: 'true' },
-      { name: 'car3', value: '全顺' },
-      { name: 'car4', value: '厢货' }
+      { name: '小面', value: 'car1', checked: 'true' },
+      { name: '金杯', value: 'car2' },
+      { name: '全顺', value: 'car3'},
+      { name: '厢货', value: 'car4' }
     ]
   },
   //事件处理函数
   radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
+    let value = e.detail.value;
+    if (value == 'car1') {
+     this.data.carExplainTop = '起步208元(10公里)，超出5元/公里';
+     this.data.carExplainBottom = '容量：可容纳10个包裹，单个包裹限重15公斤';
+    }
+    else if (value == 'car2') {
+      this.data.carExplainTop = '起步288元(10公里)，超出6元/公里';
+      this.data.carExplainBottom = '容量：可容纳15个包裹，单个包裹限重15公斤';
+    }
+    else if (value == 'car3') {
+      this.data.carExplainTop = '起步388元(10公里)，超出8元/公里';
+      this.data.carExplainBottom = '可容纳20个包裹，单个包裹限重15公斤';
+    }
+    else if (value == 'car4') {
+      this.data.carExplainTop = '起步1288元(10公里)，超出10元/公里';
+      this.data.carExplainBottom = '可容纳30个包裹，单个包裹限重15公斤';
+    }
+    this.setData({
+      carExplainTop: this.data.carExplainTop, 
+      carExplainBottom: this.data.carExplainBottom
+    });
   },
   openCarExplain: function(){
     console.log('车型说明')
