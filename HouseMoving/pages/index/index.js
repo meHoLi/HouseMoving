8ea8b49c83220aab039b5316515094545551e272
meHoLi <346857553@qq.com>
2018-06-20 10:02:09 +0800

    初识小程序

--- HouseMoving/pages/index/index.js
+++ HouseMoving/pages/index/index.js
@@ -4,51 +4,20 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    carExplainTop:'计价：起步1288元(10公里)，超出10元/公里',
+    carExplainBottom: '容量：可容纳30个包裹，单个包裹限重100kg',
+    items: [
+      { name: 'car1', value: '小面' },
+      { name: 'car2', value: '金杯', checked: 'true' },
+      { name: 'car3', value: '全顺' },
+      { name: 'car4', value: '厢货' }
+    ]
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  openCarExplain: function(){
+    console.log('车型说明')
   }
 })
