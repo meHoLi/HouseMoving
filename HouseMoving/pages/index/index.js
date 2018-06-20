7e453e7043df328dc5d6033a744c5c306b4e5e37
meHoLi <346857553@qq.com>
2018-06-20 19:38:57 +0800

    高德自动补全

--- HouseMoving/pages/index/index.js
+++ HouseMoving/pages/index/index.js
@@ -1,9 +1,11 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var amapFile = require('../../utils/amap-wx.js'); 
+var markersData = [];
 Page({
   data: {
+    //车型计价和容量
     carExplainTop:'计价：起步208元(10公里)，超出5元/公里',
     carExplainBottom: '容量：可容纳10个包裹，单个包裹限重15公斤',
     items: [
@@ -11,9 +13,49 @@ Page({
       { name: '金杯', value: 'car2' },
       { name: '全顺', value: 'car3'},
       { name: '厢货', value: 'car4' }
-    ]
+    ],
+    //下拉框
+    nickName: "",
+    avatarUrl: "",
+    casArray: ['电梯-免费', '1楼-免费', '2楼-加收20元', '3楼-加收30元', '4楼-加收40元', '5楼-加收50元', '6楼-加收60元', '7楼-加收70元','8楼-加收80元'],
+    userName: '',
+    mobile: '',
+    Gender: 'female',
+    casIndex: 0,
+    //高德
+    tips: {},
+    keywords:''
   },
-  //事件处理函数
+
+  onLoad: function () {
+
+  },
+  bindInput: function (e) {
+    var that = this;
+    var keywords = e.detail.value;
+    var myAmapFun = new amapFile.AMapWX({ key: 'ab3b9da6a118e991647e3b91606d6fba' });
+    myAmapFun.getInputtips({
+      keywords: keywords,
+      location: '',
+      success: function (data) {
+        if (data && data.tips) {
+          that.setData({
+            tips: data.tips
+          });
+        }
+
+      }
+    })
+  },
+  bindSearch: function (e) {
+    this.setData({
+      keywords: e.target.dataset.keywords
+    })
+    //wx.redirectTo({
+   //   url: url
+    //})
+  },
+  //车型事件处理函数
   radioChange: function (e) {
     let value = e.detail.value;
     if (value == 'car1') {
@@ -39,5 +81,17 @@ Page({
   },
   openCarExplain: function(){
     console.log('车型说明')
+  },
+   //下拉框事件
+  bindCasPickerChange: function (e) {
+    console.log('乔丹选的是', this.data.casArray[e.detail.value])
+    if (e.detail.value == 4) {
+      this.setData({ reply: true })
+    } else {
+      this.setData({ reply: false })
+    }
+    this.setData({
+      casIndex: e.detail.value
+    })
   }
 })
